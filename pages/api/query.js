d1cf97bcc9c262e64cb5ae6020d9257ff680d2c9
Cheng <berialcheng@gmail.com>
2022-07-13 11:56:11 +0800

    add api

---
+++ pages/api/query.js
@@ -0,0 +1,17 @@
+const braintree = require("braintree");
+
+const gateway = braintree.connect({
+  environment:  braintree.Environment.Sandbox,
+  merchantId:   'th5v3d27kbgwmy93',
+  publicKey:    '5gxr2k9s8sfrvt9n',
+  privateKey:   '1a6656f87fa1eeb51a31dd030c1f1f7b'
+});
+
+
+export default function handler(req, res) {
+    const stream = gateway.transaction.search((search) => {}, (err, response) => {
+        response.each((err, transaction) => {
+            console.log(transaction);
+        });
+    });
+}
---
+++ pages/api/transaction.js
@@ -0,0 +1,23 @@
+const braintree = require("braintree");
+
+const gateway = braintree.connect({
+  environment:  braintree.Environment.Sandbox,
+  merchantId:   'th5v3d27kbgwmy93',
+  publicKey:    '5gxr2k9s8sfrvt9n',
+  privateKey:   '1a6656f87fa1eeb51a31dd030c1f1f7b'
+});
+
+export default function handler(req, res) {
+  gateway.transaction.sale({
+    amount: "10.00",
+    paymentMethodNonce: nonceFromTheClient,
+    deviceData: deviceDataFromTheClient,
+    options: {
+      submitForSettlement: true
+    },
+    //billing: {
+      //postalCode: postalCodeFromTheClient
+    //}
+  }, (err, result) => {
+  });
+}
