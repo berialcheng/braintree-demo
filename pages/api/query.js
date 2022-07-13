fc82083d1f52ee4f9b7059fef2b702b27124fe56
Cheng <berialcheng@gmail.com>
2022-07-13 13:32:44 +0800

    update api

--- pages/api/query.js
+++ pages/api/query.js
@@ -1,21 +1,31 @@
 const braintree = require("braintree");
+const stream = require("stream");
 
-const gateway = braintree.connect({
+const gateway = new braintree.BraintreeGateway({
   environment:  braintree.Environment.Sandbox,
   merchantId:   'th5v3d27kbgwmy93',
   publicKey:    '5gxr2k9s8sfrvt9n',
   privateKey:   '1a6656f87fa1eeb51a31dd030c1f1f7b'
 });
 
-export default function handler(req, res) {
-    const stream = gateway.transaction.search((search) => {}, (err, response) => {
-        // response.each((err, transaction) => {
-        //     console.log(transaction);
-        // });
-        if(err) {
-            res.status(500).json(err)
-        } else {
-            res.status(200).json(response)
-        }
-    });
+export default async function handler(req, res) {
+    return new Promise((resolve, reject) => {
+        const stream = gateway.transaction.search((search) => {}, (err, response) => {
+            var list = []
+            var eachTransactions = new Promise((resolve, reject) => {
+                response.each((err, transaction) => {
+                    list.push(transaction)  
+                })
+            })
+            await eachTransactions;
+            console.log(list)
+            if(err) {
+                res.status(500).json(err)
+                reject("FAILURE")
+            } else {
+                res.status(200).json(response)
+                resolve("SUCCESS")
+            }
+        });
+    })
 }
--- pages/api/transaction.js
+++ pages/api/transaction.js
@@ -1,28 +1,36 @@
 const braintree = require("braintree");
 
-const gateway = braintree.connect({
+const gateway = new braintree.BraintreeGateway({
   environment:  braintree.Environment.Sandbox,
   merchantId:   'th5v3d27kbgwmy93',
   publicKey:    '5gxr2k9s8sfrvt9n',
   privateKey:   '1a6656f87fa1eeb51a31dd030c1f1f7b'
 });
 
-export default function handler(req, res) {
-  gateway.transaction.sale({
-    amount: "10.00",
-    paymentMethodNonce: nonceFromTheClient,
-    deviceData: deviceDataFromTheClient,
-    options: {
-      submitForSettlement: true
-    },
-    //billing: {
-      //postalCode: postalCodeFromTheClient
-    //}
-  }, (err, result) => {
-    if(err) {
-      res.status(500).json(err)
-    } else {
-      res.status(200).json(result)
-    }
-  });
+export default async function handler(req, res) {
+  return new Promise((resolve, reject) => {
+    // const amount = req.body.amount
+    // const paymentMethodNonce = req.body.paymentMethodNonce
+    // const deviceData = req.body.deviceData
+
+    gateway.transaction.sale({
+      amount: req.body.amount,
+      paymentMethodNonce: req.body.paymentMethodNonce,
+      deviceData: req.body.deviceData,
+      options: {
+        submitForSettlement: req.body.submitForSettlement || true
+      },
+      //billing: {
+        //postalCode: postalCodeFromTheClient
+      //}
+    }, (err, result) => {
+      if(err) {
+        res.status(500).json(err)
+        reject("FAILURE")
+      } else {
+        res.status(200).json(result)
+        resolve("SUCCESS")
+      }
+    });
+  })
 }
