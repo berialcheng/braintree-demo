cbe58753923070eeb4f5bdc9ffb5edc05e75369d
Cheng <berialcheng@gmail.com>
2022-12-20 09:43:03 +0000

    feat(api): udpate hook

--- pages/api/hook.js
+++ pages/api/hook.js
@@ -1,7 +1,7 @@
 
 
 export default function handler(req, res) {
-    console.log(req.query)
-    console.log(req.body)
-    res.status(200).json({'response': ['accepted']});
+    console.log("req.query", req.query)
+    console.log("req.body", req.body)
+    res.status(200).json({'response': '[accepted]'})
 }
