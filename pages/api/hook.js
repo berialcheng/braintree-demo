90fc550435d81d389ebf4395341fca1f0642eb1b
Cheng <berialcheng@gmail.com>
2022-12-20 09:58:41 +0000

    feat(api): update hook

--- pages/api/hook.js
+++ pages/api/hook.js
@@ -2,6 +2,14 @@
 
 export default function handler(req, res) {
     console.log("req.query", req.query)
+
+    if (req.body && req.body.notificationItems) {
+        var notificationItems = req.body.notificationItems;
+        notificationItems.map(n => {
+            return n
+        })
+    }
+
     console.log("req.body", req.body)
     res.status(200).json({'response': '[accepted]'})
 }
